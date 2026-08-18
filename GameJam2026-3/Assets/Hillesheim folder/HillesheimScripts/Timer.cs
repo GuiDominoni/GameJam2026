bb44a28e3c2203240ced2dfe192daac6f40b3802
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2026-08-18 14:09:09 -0300

    parando o tempo

--- GameJam2026-3/Assets/Hillesheim folder/HillesheimScripts/Timer.cs
+++ GameJam2026-3/Assets/Hillesheim folder/HillesheimScripts/Timer.cs
@@ -5,12 +5,23 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
     private float timePassing;
+    private bool isDone = false;
+    private void Awake()
+    {
+        isDone = false;
+    }
     void Update()
     {
+        if (isDone == true)
+            return;
         timePassing += Time.deltaTime;
         int minutes = Mathf.FloorToInt(timePassing / 60f);
         int seconds = Mathf.FloorToInt(timePassing % 60f);
         int milisseconds = Mathf.FloorToInt((timePassing * 100f) % 100f);
         timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milisseconds);
+    }
+    public void StopTimer()
+    {
+        isDone = true;
     }
 }
